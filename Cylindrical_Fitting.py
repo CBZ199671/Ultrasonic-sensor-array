25e1c06f45763b60d947d32feda108dbe1fc1274
CBZ199671 <59392867+CBZ199671@users.noreply.github.com>
2022-03-23 16:12:24 +0800

    Add files via upload
    Ransac_Cylindrical_Fitting

--- Cylindrical_Fitting.py
+++ Cylindrical_Fitting.py
@@ -1,151 +1,48 @@
 import numpy as np
-from scipy import optimize
 from scipy.optimize import leastsq
-from scipy.optimize import minimize
 import pandas as pd
 from matplotlib.pyplot import title, xlabel, ylabel
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import math
+import random
 
 #-*- coding: utf-8 -*-
 myfont = FontProperties(fname=r"C:\Windows\Fonts\simsun.ttc", size=14)
 
-def xclinerrors(para, points): 
-    """法向量x矢量最大，圆柱面拟合误差"""
-    # points为由点集构成的n*3矩阵，每一行为一个点的x,y,z坐标值
-    x0 = 0
-    y0, z0, a0, b0, c0, r0 = para
-    return (points[:, 0] - x0) ** 2 + (points[:, 1] - y0) ** 2 + (points[:, 2] - z0) ** 2 - (
-            a0 * (points[:, 0] - x0) + b0 * (points[:, 1] - y0) + c0 * (points[:, 2] - z0)) ** 2 - r0 ** 2
+def rodrigues_rot(P, n0, n1):
+    """ 
+    Rotate a set of point between two normal vectors using Rodrigues' formula. 
 
+    :param P: Set of points `np.array (N,3)`.
+    :param n0: Orign vector `np.array (1,3)`.
+    :param n1: Destiny vector `np.array (1,3)`.
 
-def yclinerrors(para, points):
-    """法向量y矢量最大，圆柱面拟合误差"""
-    y0 = 0
-    x0, z0, a0, b0, c0, r0 = para
-    return (points[:, 0] - x0) ** 2 + (points[:, 1] - y0) ** 2 + (points[:, 2] - z0) ** 2 - (
-            a0 * (points[:, 0] - x0) + b0 * (points[:, 1] - y0) + c0 * (points[:, 2] - z0)) ** 2 - r0 ** 2
+    :returns: Set of points P, but rotated `np.array (N, 3)`
 
+    ---
+    """
 
-def zclinerrors(para, points):
-    """法向量z矢量最大，圆柱面拟合误差"""
-    z0 = 0
-    x0, y0, a0, b0, c0, r0 = para
-    return (points[:, 0] - x0) ** 2 + (points[:, 1] - y0) ** 2 + (points[:, 2] - z0) ** 2 - (
-            a0 * (points[:, 0] - x0) + b0 * (points[:, 1] - y0) + c0 * (points[:, 2] - z0)) ** 2 - r0 ** 2
-
-def direction(theta, phi):
-    '''Return the direction vector of a cylinder defined
-    by the spherical coordinates theta and phi.
-    '''
-    return np.array([np.cos(phi) * np.sin(theta), np.sin(phi) * np.sin(theta),
-                     np.cos(theta)])
-
-def projection_matrix(w):
-    '''Return the projection matrix  of a direction w.'''
-    return np.identity(3) - np.dot(np.reshape(w, (3,1)), np.reshape(w, (1, 3)))
-
-def skew_matrix(w):
-    '''Return the skew matrix of a direction w.'''
-    return np.array([[0, -w[2], w[1]],
-                     [w[2], 0, -w[0]],
-                     [-w[1], w[0], 0]])
-
-def calc_A(Ys):
-    '''Return the matrix A from a list of Y vectors.'''
-    return sum(np.dot(np.reshape(Y, (3,1)), np.reshape(Y, (1, 3)))
-            for Y in Ys)
-
-def calc_A_hat(A, S):
-    '''Return the A_hat matrix of A given the skew matrix S'''
-    return np.dot(S, np.dot(A, np.transpose(S)))
-
-def preprocess_data(Xs_raw):
-    '''Translate the center of mass (COM) of the data to the origin.
-    Return the prossed data and the shift of the COM'''
-    n = len(Xs_raw)
-    Xs_raw_mean = sum(X for X in Xs_raw) / n
-
-    return [X - Xs_raw_mean for X in Xs_raw], Xs_raw_mean
-
-def G(w, Xs):
-    '''Calculate the G function given a cylinder direction w and a
-    list of data points Xs to be fitted.'''
-    n = len(Xs)
-    P = projection_matrix(w)
-    Ys = [np.dot(P, X) for X in Xs]
-    A = calc_A(Ys)
-    A_hat = calc_A_hat(A, skew_matrix(w))
-
+    # If P is only 1d array (coords of single point), fix it to be matrix
+    P = np.asarray(P)
+    if P.ndim == 1:
+        P = P[np.newaxis,:]
     
-    u = sum(np.dot(Y, Y) for Y in Ys) / n
-    v = np.dot(A_hat, sum(np.dot(Y, Y) * Y for Y in Ys)) / np.trace(np.dot(A_hat, A))
-
-    return sum((np.dot(Y, Y) - u - 2 * np.dot(Y, v)) ** 2 for Y in Ys)
-
-def C(w, Xs):
-    '''Calculate the cylinder center given the cylinder direction and 
-    a list of data points.
-    '''
-    n = len(Xs)
-    P = projection_matrix(w)
-    Ys = [np.dot(P, X) for X in Xs]
-    A = calc_A(Ys)
-    A_hat = calc_A_hat(A, skew_matrix(w))
-
-    return np.dot(A_hat, sum(np.dot(Y, Y) * Y for Y in Ys)) / np.trace(np.dot(A_hat, A))
-
-def r(w, Xs):
-    '''Calculate the radius given the cylinder direction and a list
-    of data points.
-    '''
-    n = len(Xs)
-    P = projection_matrix(w)
-    c = C(w, Xs)
-
-    return np.sqrt(sum(np.dot(c - X, np.dot(P, c - X)) for X in Xs) / n)
-
-def fit(data, guess_angles=None):
-    '''Fit a list of data points to a cylinder surface. The algorithm implemented
-    here is from David Eberly's paper "Fitting 3D Data with a Cylinder" from 
-    https://www.geometrictools.com/Documentation/CylinderFitting.pdf
-
-    Arguments:
-        data - A list of 3D data points to be fitted.
-        guess_angles[0] - Guess of the theta angle of the axis direction
-        guess_angles[1] - Guess of the phi angle of the axis direction
-    
-    Return:
-        Direction of the cylinder axis
-        A point on the cylinder axis
-        Radius of the cylinder
-        Fitting error (G function)
-    '''
-    Xs, t = preprocess_data(data)  
-
-    # Set the start points
-
-    start_points = [(0, 0), (np.pi / 2, 0), (np.pi / 2, np.pi / 2)]
-    if guess_angles:
-        start_points = guess_angles
-
-    # Fit the cylinder from different start points 
-
-    best_fit = None
-    best_score = float('inf')
-
-    for sp in start_points:
-        fitted = minimize(lambda x : G(direction(x[0], x[1]), Xs),
-                    sp, method='Powell', tol=1e-6)
-
-        if fitted.fun < best_score:
-            best_score = fitted.fun
-            best_fit = fitted
-
-    w = direction(best_fit.x[0], best_fit.x[1])
-
-    return w, C(w, Xs) + t, r(w, Xs), best_fit.fun 
+    # Get vector of rotation k and angle theta
+    n0 = n0/np.linalg.norm(n0)
+    n1 = n1/np.linalg.norm(n1)
+    k = np.cross(n0,n1)
+    P_rot = np.zeros((len(P),3))
+    if(np.linalg.norm(k)!=0):
+        k = k/np.linalg.norm(k)
+        theta = np.arccos(np.dot(n0,n1))
+        
+        # Compute rotated points
+        for i in range(len(P)):
+            P_rot[i] = P[i]*np.cos(theta) + np.cross(k,P[i])*np.sin(theta) + k*np.dot(k,P[i])*(1-np.cos(theta))
+    else:
+        P_rot = P
+    return P_rot
 
 def rotation_matrix_from_axis_and_angle(u, theta):
     '''Calculate a rotation matrix from an axis and an angle.'''
@@ -160,6 +57,114 @@ def rotation_matrix_from_axis_and_angle(u, theta):
                      [y * x * (1 - c) + z * s, c + y**2 * (1 - c), y * z * (1 - c) - x * s ],
                      [z * x * (1 - c) - y * s, z * y * (1 - c) + x * s, c + z**2 * (1 - c) ]])
 
+
+class Cylinder:
+    """ 
+    !!! warning
+        The cylinder RANSAC in this library works! but does not present good results on real data on the current version.
+        We are working to make a better algorithim using normals. If you want to contribute, please create a MR on github.
+        You'll be our hero! 
+
+    Implementation for cylinder RANSAC.
+
+    This class finds a infinite height cilinder and returns the cylinder axis, center and radius. 
+
+    ---
+    """
+    
+
+    def __init__(self):
+        self.inliers = []
+        self.center = []
+        self.axis = []
+        self.radius = 0
+
+    def fit(self, pts, thresh=0.2, maxIteration=10000):
+        """ 
+        Find the parameters (axis and radius) defining a cylinder. 
+
+        :param pts: 3D point cloud as a numpy array (N,3).
+        :param thresh: Threshold distance from the cylinder hull which is considered inlier.
+        :param maxIteration: Number of maximum iteration which RANSAC will loop over.
+
+        :returns: 
+        - `center`: Center of the cylinder np.array(1,3) which the cylinder axis is passing through.
+        - `axis`: Vector describing cylinder's axis np.array(1,3).
+        - `radius`: Radius of cylinder.
+        - `inliers`: Inlier's index from the original point cloud.
+        ---
+        """
+
+        n_points = pts.shape[0]
+        best_eq = []
+        best_inliers = []
+
+        for it in range(maxIteration):
+
+            # Samples 3 random points 
+            id_samples = random.sample(range(1, n_points-1), 3)
+            pt_samples = pts[id_samples]
+
+            # We have to find the plane equation described by those 3 points
+            # We find first 2 vectors that are part of this plane
+            # A = pt2 - pt1
+            # B = pt3 - pt1
+
+            vecA = pt_samples[1,:] - pt_samples[0,:]
+            vecA_norm = vecA / np.linalg.norm(vecA)
+            vecB = pt_samples[2,:] - pt_samples[0,:]
+            vecB_norm = vecB / np.linalg.norm(vecB)
+
+            # Now we compute the cross product of vecA and vecB to get vecC which is normal to the plane
+            vecC = np.cross(vecA_norm, vecB_norm)
+            vecC = vecC / np.linalg.norm(vecC)
+
+            # Now we calculate the rotation of the points with rodrigues equation
+            P_rot = rodrigues_rot(pt_samples, vecC, [0,0,1])
+
+            # Find center from 3 points
+            # http://paulbourke.net/geometry/circlesphere/
+            # Find lines that intersect the points
+            # Slope:
+            ma = 0
+            mb = 0
+            while(ma == 0):
+                ma = (P_rot[1, 1]-P_rot[0, 1])/(P_rot[1, 0]-P_rot[0, 0])
+                mb = (P_rot[2, 1]-P_rot[1, 1])/(P_rot[2, 0]-P_rot[1, 0])
+                if(ma == 0):
+                    P_rot = np.roll(P_rot,-1,axis=0)
+                else:
+                    break
+
+            # Calulate the center by verifying intersection of each orthogonal line
+            p_center_x = (ma*mb*(P_rot[0, 1]-P_rot[2, 1]) + mb*(P_rot[0, 0]+P_rot[1, 0]) - ma*(P_rot[1, 0]+P_rot[2, 0]))/(2*(mb-ma))
+            p_center_y = -1/(ma)*(p_center_x - (P_rot[0, 0]+P_rot[1, 0])/2)+(P_rot[0, 1]+P_rot[1, 1])/2
+            p_center = [p_center_x, p_center_y, 0]
+            radius = np.linalg.norm(p_center - P_rot[0, :])
+
+            # Remake rodrigues rotation
+            center = rodrigues_rot(p_center, [0,0,1], vecC)[0]
+
+            # Distance from a point to a line
+            pt_id_inliers = [] # list of inliers ids
+            vecC_stakado =  np.stack([vecC]*n_points,0)
+            dist_pt = np.cross(vecC_stakado, (center- pts))
+            dist_pt = np.linalg.norm(dist_pt, axis=1)
+
+
+            # Select indexes where distance is biggers than the threshold
+            pt_id_inliers = np.where(np.abs(dist_pt-radius) <= thresh)[0]
+
+            if(len(pt_id_inliers) > len(best_inliers)):
+                best_inliers = pt_id_inliers
+                self.inliers = best_inliers
+                self.center = center
+                self.axis = vecC
+                self.radius = radius
+
+        return self.center, self.axis, self.radius,  self.inliers
+
+
 # 读取数据
 UL_step = []
 theta = []
@@ -168,12 +173,12 @@ new_ULx = []
 new_ULy = []
 ts = []
 
-dfx = pd.read_excel('bottle(25mm)_1.xlsx',usecols=[1],names=None)
+dfx = pd.read_excel('bottle(300mm)_10.xlsx',usecols=[1],names=None)
 dfx = dfx.values.tolist() 
 for i in dfx:
     UL_step.append(i[0])
 
-dft = pd.read_excel('bottle_time(25mm)_1.xlsx',usecols=[1],names=None)
+dft = pd.read_excel('bottle_time(300mm)_10.xlsx',usecols=[1],names=None)
 dft = dft.values.tolist() 
 for j in dft:
     ts.append(j[0])
@@ -211,44 +216,12 @@ points = np.append(xys, zs, axis=1)
 
 
 
+cil = Cylinder()
 
- # 计算点集的x,y,z均值，x，y的范围，用于拟合参数初值
-xm = np.mean(points[:,0])    # 特征点集x均值
-ym = np.mean(points[:,1])
-zm = np.mean(points[:,2])         
-xrange = np.max(points[:,0]) - np.min(points[:,0])    # 特征点集x范围
-yrange = np.max(points[:,1]) - np.min(points[:,1])
-
-
-
-cnv, C_fit, r_fit, fit_err = fit(points)
-
-print('中心点%s 圆柱法向量%s 半径%s' %(cnv, C_fit, r_fit))
-
-
-# 最小二乘拟合 
-# x分量最大，则法向量必然与yz平面相交，轴点初值中x可设为0
-# cnv为公法线
-if cnv[0] >= cnv[1] and cnv[0] >= cnv[2]:   
-    tparac = leastsq(xclinerrors, [ym, zm, cnv[0], cnv[1], cnv[2], yrange/2], points,
-                                              full_output=1)
-    parac = np.array([0, tparac[0][0], tparac[0][1], tparac[0][2], tparac[0][3], tparac[0][4], tparac[0][5]])
-    # 圆度误差均值
-    ec = np.mean(np.abs(tparac[2]['fvec'])) / parac[6]
-    
-if cnv[1] >= cnv[0] and cnv[1] >= cnv[2]:
-    tparac = leastsq(yclinerrors, [xm, zm, cnv[0], cnv[1], cnv[2], xrange/2], points,
-                                              full_output=1)
-    parac = np.array([tparac[0][0], 0, tparac[0][1], tparac[0][2], tparac[0][3], tparac[0][4], tparac[0][5]])
-    ec = np.mean(np.abs(tparac[2]['fvec'])) / parac[6]
-    
-if cnv[2] >= cnv[0] and cnv[2] >= cnv[1]:
-    tparac = leastsq(zclinerrors, [xm, ym, cnv[0], cnv[1], cnv[2], (xrange + yrange) / 2],
-                                             points,full_output=1)
-    parac = np.array([tparac[0][0], tparac[0][1], 0, tparac[0][2], tparac[0][3], tparac[0][4], tparac[0][5]])
-    ec = np.mean(np.abs(tparac[2]['fvec'])) / parac[6]
-
-
+center, normal, radius,  inliers = cil.fit(points, thresh=0.05)
+print("center: "+str(center))
+print("radius: "+str(radius))
+print("vecC: "+str(normal))
 
 fig = plt.figure()
 ax = fig.gca(projection="3d")
@@ -257,15 +230,12 @@ ax.scatter(points.T[0],points.T[1],points.T[2],zdir="z",marker="o",s=4)
 
 
 ax.set(xlabel="X",ylabel="Y",zlabel="Z")
-ax.set_title(u'柱面点云及最小二乘拟合图',fontproperties=myfont)
-
-
-
+ax.set_title(u'柱面点云及Ransac拟合图',fontproperties=myfont)
 
 # Get the transformation matrix
 
-theta = np.arccos(np.dot(cnv, np.array([0, 0, 1])))
-phi = np.arctan2(cnv[1], cnv[0])
+theta = np.arccos(np.dot(normal, np.array([0, 0, 1])))
+phi = np.arctan2(normal[1], normal[0])
 
 M = np.dot(rotation_matrix_from_axis_and_angle(np.array([0, 0, 1]), phi),
             rotation_matrix_from_axis_and_angle(np.array([0, 1, 0]), theta))
@@ -273,15 +243,15 @@ M = np.dot(rotation_matrix_from_axis_and_angle(np.array([0, 0, 1]), phi),
 # Plot the cylinder surface
 
 delta = np.linspace(-np.pi, np.pi, 200)
-z = np.linspace(-10, 10, 200)
+z = np.linspace(0, 12, 200)
 
 Delta, Z = np.meshgrid(delta, z)
-X = r_fit * np.cos(Delta)
-Y = r_fit * np.sin(Delta)
+X = radius * np.cos(Delta)
+Y = radius * np.sin(Delta)
 
 for i in range(len(X)):
     for j in range(len(X[i])):
-        p = np.dot(M, np.array([X[i][j], Y[i][j], Z[i][j]])) + C_fit
+        p = np.dot(M, np.array([X[i][j], Y[i][j], Z[i][j]])) + center
 
         X[i][j] = p[0]
         Y[i][j] = p[1]
